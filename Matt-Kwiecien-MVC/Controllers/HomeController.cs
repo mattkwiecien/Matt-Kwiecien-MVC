79bae48d7aa9c2fafe6ee7a71746734374adbcc3
Matthew Kwiecien <mkwieci2@gmail.com>
2019-01-20 12:30:21 -0600

    Another style overhaul.

--- Matt-Kwiecien-MVC/Controllers/HomeController.cs
+++ Matt-Kwiecien-MVC/Controllers/HomeController.cs
@@ -30,10 +30,6 @@ namespace Matt_Kwiecien_MVC.Controllers {
 			return View();
 		}
 
-		public IActionResult Privacy() {
-			return View();
-		}
-
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error() {
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
--- Matt-Kwiecien-MVC/wwwroot/js/site.js
+++ Matt-Kwiecien-MVC/wwwroot/js/site.js
@@ -1,4 +1,29 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+$('.scroll-to').click(function () {
+    var anchor = $(this).attr("dest");
+    $('.link-wrap').removeClass('visible');
 
-// Write your JavaScript code.
+    $('html, body').animate({
+        scrollTop: $('#' + anchor).offset().top
+    }, 400);
+});
+
+
+// NAV POSITION
+var navPos = ($('.main-header')[0].getBoundingClientRect().top - document.body.getBoundingClientRect().top) - 10;
+var lastPos = 0;
+
+$(window).on('scroll', function () {
+
+    var pos = $(window).scrollTop();
+
+    if (pos >= navPos && lastPos < pos) {
+        $('.main-header').addClass('fixed');
+        $('.main-footer').addClass('fixed');
+    }
+    if (pos + $('.main-footer').height() < navPos && lastPos > pos) {
+        $('.main-header').removeClass('fixed');
+        $('.main-footer').removeClass('fixed');
+    }
+    lastPos = pos;
+});
